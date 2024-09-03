1ef5b1495c86ba3dc928d218f3e0eb7d63f81ef8
mnibor <marcelorobin@gmail.com>
2024-09-03 17:58:28 -0300

    s

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="bg-slate-500 py-6 text-slate-200">
+    <nav className="bg-slate-500 py-6 text-gray-200">
       <div className="mx-auto flex w-full max-w-4xl items-center justify-between px-8">
         <div>
           <h5>CASHAPPS</h5>
